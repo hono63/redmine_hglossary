499e148caaea1e549c05fd9f1779ec6aeac93213
hono63 <hono6363@yahoo.co.jp>
2018-03-04 19:04:22 +0900

    編集成功。

--- app/controllers/acronyms_controller.rb
+++ app/controllers/acronyms_controller.rb
@@ -4,30 +4,47 @@ class AcronymsController < ApplicationController
     @acros = Acronyms.all
   end
 
+  # model作成時の画面アクション
   def new
     @acro = Acronyms.new
   end
 
+  # 実際にmodel作成するアクション
   def create
-  #@acro = Acronyms.new(params[:acronyms]) # <=これは何故か ForbiddenAttributesError が出る。
-  #ac = params[:acronyms].permit(:acronym, :definition, :origin)
-  #@acro = Acronyms.new(ac)
-  @acro = Acronyms.new(get_params)
- 
-  @acro.save
-  redirect_to @acro
-  # ↓ これはPOSTで何が送られているか表示する。
-  #render plain: params[:acronyms].inspect
+    # ↓これは ForbiddenAttributesError が出る。
+    #@acro = Acronyms.new(params[:acronyms]) 
+    # ↓これでも可能。
+    #ac = params[:acronyms].permit(:acronym, :definition, :origin)
+    #@acro = Acronyms.new(ac)
+    # ↓これがrails公式のやり方。
+    @acro = Acronyms.new(get_params)
+  
+    @acro.save
+    redirect_to @acro
+    # ↓ これはPOSTで何が送られているか表示するだけ。
+    #render plain: params[:acronyms].inspect
   end
 
   def show
     @acro = Acronyms.find(params[:id])
   end
 
+  # model編集時の画面アクション
   def edit 
     @acro = Acronyms.find(params[:id])
   end
 
+  # 実際にmodelをupdateするアクション
+  def update
+    @acro = Acronyms.find(params[:id])
+  
+    if @acro.update(get_params)
+      redirect_to @acro
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def get_params
     params.require(:acronyms).permit(:acronym, :definition, :origin)
